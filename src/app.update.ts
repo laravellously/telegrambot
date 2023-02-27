e37017c0f90501a3dd7ac65d925f062ab0617453
laravellously <martins.michael71@gmail.com>
2023-02-27 20:22:56 +0100

    Logger

--- src/app.update.ts
+++ src/app.update.ts
@@ -1,10 +1,14 @@
+import { Logger } from '@nestjs/common';
 import { Update, Ctx, Start, Help, On, Hears } from 'nestjs-telegraf';
 import { Context } from 'telegraf';
 
 @Update()
 export class AppUpdate {
+  private readonly logger = new Logger(AppUpdate.name);
+
   @Start()
   async start(@Ctx() ctx: Context) {
+    this.logger.log('Start Command Called');
     await ctx.reply('Welcome');
   }
 
